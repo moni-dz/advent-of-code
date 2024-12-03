b5f969fe3b07b10ba228b23033e73d677a343ad7
moni <lythe1107@gmail.com>
2024-12-04 00:01:05 +0800

    day 3

---
+++ 2024/3.clj
@@ -0,0 +1,26 @@
+(defn process-muls [input]
+  (->> input
+       (re-seq #"mul\(([-+]?\d+),([-+]?\d+)\)")
+       (map (fn [[_ x y]] (* (parse-long x) (parse-long y))))
+       (reduce +)))
+
+(defn process-instructions [input]
+  (let [tokens (re-seq #"do\(\)|don't\(\)|mul\([0-9]+,[0-9]+\)" input)]
+    (loop [tokens tokens
+           total 0
+           enabled? true]
+      (if-let [token (first tokens)]
+        (case token
+          "do()" (recur (rest tokens) total true)
+          "don't()" (recur (rest tokens) total false)
+          (recur (rest tokens)
+                 (if enabled?
+                   (+ total (reduce * (map parse-long (re-seq #"\d+" token))))
+                   total)
+                 enabled?))
+        total))))
+
+(let [data (slurp "3.txt")]
+  [(process-muls data) (process-instructions data)])
+
+
