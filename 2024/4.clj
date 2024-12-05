10f9e180b060f85afa4da1dcffc7d3b365d605df
moni <lythe1107@gmail.com>
2024-12-05 13:55:18 +0800

    day 5

--- 2024/4.clj
+++ 2024/4.clj
@@ -1,65 +1,47 @@
 (require '[clojure.string :as str])
 
-(defn in-bounds? [grid x y]
-  (and (>= x 0)
-       (>= y 0)
-       (< y (count grid))
-       (< x (count (first grid)))))
+(defn char-at [grid x y]
+  (when (and (>= x 0) (>= y 0)
+             (< y (count grid))
+             (< x (count (first grid))))
+    (get-in grid [y x])))
 
-(defn char-at [grid x y & {:keys [default] :or {default \.}}]
-  (if (in-bounds? grid x y)
-    (get-in grid [y x])
-    default))
-
-(def directions
-  [[0 1] [1 0] [1 1] [1 -1] [0 -1] [-1 0] [-1 -1] [-1 1]])
+(def directions [[0 1] [1 0] [1 1] [1 -1] [0 -1] [-1 0] [-1 -1] [-1 1]])
 
 (defn matches-word? [grid x y [dx dy]]
   (when (= (char-at grid x y) \X)
-    (let [word "XMAS"]
-      (every? (fn [i]
-                (= (nth word i)
-                   (char-at grid
-                            (+ x (* i dx))
-                            (+ y (* i dy)))))
-              (range 1 (count word))))))
+    (every? #(= (nth "XMAS" %)
+                (or (char-at grid (+ x (* % dx)) (+ y (* % dy))) \.))
+            (range 1 4))))
 
 (defn count-xmas [grid]
-  (let [grid (mapv vec grid)
-        height (count grid)
-        width (count (first grid))]
-    (->> (for [x (range width)
-               y (range height)]
-           [x y])
-         (mapcat (fn [pos] (map #(conj pos %) directions)))
-         (filter (fn [[x y dir]] (matches-word? grid x y dir)))
+  (let [grid (mapv vec grid)]
+    (->> (for [x (range (count (first grid)))
+               y (range (count grid))
+               d directions]
+           (matches-word? grid x y d))
+         (filter identity)
          count)))
 
 (def mas-patterns #{[\M \A \S] [\S \A \M]})
 
-(defn valid-diagonal? [grid row col [dx dy]]
-  (mas-patterns [(char-at grid (- row dy) (- col dx))
-                 \A
-                 (char-at grid (+ row dy) (+ col dx))]))
-
 (defn x-pattern? [grid row col]
-  (let [height (count grid)
-        width (count (first grid))]
-    (when (and (>= row 1) (< row (dec height))
-               (>= col 1) (< col (dec width))
-               (= (char-at grid row col) \A))
-      (and (valid-diagonal? grid row col [1 1])
-           (valid-diagonal? grid row col [1 -1])))))
+  (when (and (>= row 1) (< row (dec (count grid)))
+             (>= col 1) (< col (dec (count (first grid))))
+             (= (char-at grid row col) \A))
+    (and (mas-patterns [(or (char-at grid (dec row) (dec col)) \.)
+                        \A
+                        (or (char-at grid (inc row) (inc col)) \.)])
+         (mas-patterns [(or (char-at grid (dec row) (inc col)) \.)
+                        \A
+                        (or (char-at grid (inc row) (dec col)) \.)]))))
 
 (defn count-x-mas [grid]
-  (let [height (count grid)
-        width (count (first grid))]
-    (->> (for [x (range width)
-               y (range height)]
-           [x y])
-         (filter (fn [[col row]] (x-pattern? grid row col)))
-         count)))
+  (->> (for [x (range (count (first grid)))
+             y (range (count grid))]
+         (x-pattern? grid y x))
+       (filter identity)
+       count))
 
-(let [data (->> (slurp "4.txt")
-                str/split-lines)]
+(let [data (str/split-lines (slurp "4.txt"))]
   [(count-xmas data) (count-x-mas data)])
---
+++ 2024/5.clj
@@ -0,0 +1,63 @@
+(require '[clojure.string :as str])
+
+(defn valid-sequence? [rules nums]
+  (let [valid? (set nums)
+        pos (zipmap nums (range))]
+    (every? (fn [[a b]]
+              (or (not (and (valid? a) (valid? b)))
+                  (< (pos a) (pos b))))
+            rules)))
+
+(defn create-graph [rule-pairs nums]
+  (let [valid? (set nums)]
+    (reduce (fn [g [a b]]
+              (if (and (valid? a) (valid? b))
+                (-> g
+                    (update a (fnil conj #{}) b)
+                    (update b (fnil conj #{}) nil))
+                g))
+            (zipmap nums (repeat #{}))
+            rule-pairs)))
+
+(defn topological-sort [graph]
+  (loop [result []
+         nodes (keys graph)
+         edges (update-vals graph #(disj % nil))]
+    (if (empty? nodes)
+      result
+      (if-let [node (first (filter #(empty? (edges %)) nodes))]
+        (recur (conj result node)
+               (remove #{node} nodes)
+               (update-vals edges #(disj % node)))
+        (concat result nodes)))))
+
+(defn get-middle [nums]
+  (nth nums (quot (count nums) 2)))
+
+(def input
+  (let [[order produce] (-> (slurp "5.txt")
+                            (str/split #"\n\n")
+                            ((fn [[a b]]
+                               [(map #(str/split % #"\|") (str/split a #"\n"))
+                                (map #(str/split % #",") (str/split b #"\n"))])))]
+    {:rules (set (map (fn [[a b]] [(parse-long a) (parse-long b)]) order))
+     :sequences (map #(mapv parse-long %) produce)}))
+
+(let [{:keys [rules sequences]} input
+      valid? (partial valid-sequence? rules)
+      valid-sequences (filter valid? sequences)
+      invalid-sequences (remove valid? sequences)]
+
+  [(reduce + (map get-middle valid-sequences))
+
+   (->> invalid-sequences
+        (map #(topological-sort (create-graph rules %)))
+        (map get-middle)
+        (reduce +))])
+
+
+
+
+
+
+
