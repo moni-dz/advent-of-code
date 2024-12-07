8dae7a4d2ef5818bc2e1345dc5dd35e89b4dcaa3
moni <lythe1107@gmail.com>
2024-12-07 20:24:44 +0800

    optimizations

--- src/2024/1.clj
+++ src/2024/1.clj
@@ -4,12 +4,12 @@
  [data (->> (slurp "inputs/2024/1.txt")
             str/split-lines
             (mapv #(mapv parse-long (str/split % #"\s+")))
-            (apply map vector))
+            (apply map vector))]
   [(->> data
         (map sort)
         (apply map (fn [x y] (abs (- x y))))
         (reduce +))
    (->> data
         ((fn [[xs ys]]
-           (transduce (map #(* % (count (filter #{%} ys)))) + xs))))]])
+           (transduce (map #(* % (count (filter #{%} ys)))) + xs))))])
      ; [1722302 20373490]
--- src/2024/7.clj
+++ src/2024/7.clj
@@ -1,62 +1,47 @@
 (require '[clojure.string :as str]
-         '[clojure.math :as math]
-         '[clojure.core.async :as a :refer [>!! <!!]])
+         '[clojure.math :as math])
 
-(defn evaluate [target exprs ops]
-  (reduce (fn [result [op n]]
-            (let [new-result
-                  (case op
-                    \+ (+ result n)
-                    \* (* result n)
-                    \| (+ (* result (long (math/pow 10 (count (str n))))) n))]
-              (if (> new-result target)
-                (reduced nil)
-                new-result)))
-          (first exprs)
-          (map vector ops (rest exprs))))
+(defn multiplier [n]
+  (long (math/pow 10 (int (inc (math/log10 n))))))
 
-(defn place-ops [n ops]
-  (for [i (range (int (math/pow (count ops) n)))]
-    (map #(nth ops
-               (int (mod (quot i (int (math/pow (count ops) %)))
-                         (count ops))))
-         (range n))))
+(def multiplier! (memoize multiplier))
+
+(defn apply-op [a b op]
+  (case op
+    \+ (+ a b)
+    \* (* a b)
+    \| (+ (* a (multiplier! b)) b)))
+
+(defn dfs [exprs-count exprs target ops index value]
+  (cond
+    (> value target) nil
+    (= index (dec exprs-count))
+    (when (= value target)
+      true)
+    :else
+    (some (fn [op]
+            (let [next-number (nth exprs (inc index))
+                  new-value (apply-op value next-number op)]
+              (dfs exprs-count exprs target ops (inc index) new-value)))
+          ops)))
 
 (defn solve [ops [target exprs]]
-  (some #(when-let [result (evaluate target exprs %)]
-           (= target result))
-        (place-ops (dec (count exprs)) ops)))
+  (let [exprs-count (count exprs)]
+    (when (dfs exprs-count exprs target ops 0 (first exprs))
+      target)))
 
-(defn process-chunk [ops chunk]
-  (->> chunk
-       (filter (partial solve ops))
-       (transduce (map first) + 0)))
-
-(defn parallel-solve [data ops]
-  (let [n-cores (.. Runtime getRuntime availableProcessors)
-        chunks (partition-all (/ (count data) n-cores) data)
-        result-chan (a/chan)
-        work-done (atom 0)]
-
-    (doseq [chunk chunks]
-      (a/thread
-        (let [result (process-chunk ops chunk)]
-          (>!! result-chan result)
-          (swap! work-done inc)
-          (when (= @work-done (count chunks))
-            (a/close! result-chan)))))
-
-    (loop [sum 0]
-      (if-let [value (<!! result-chan)]
-        (recur (+ sum value))
-        sum))))
+(defn solve! [data ops]
+  (->> data
+       (pmap #(solve ops %))
+       (filter some?)
+       (reduce +)))
 
 (let [data (->> (slurp "inputs/2024/7.txt")
                 str/split-lines
                 (map #(let [[n rest] (str/split % #":\s+")
                             target (parse-long n)
-                            exprs (map parse-long (str/split rest #"\s+"))]
+                            exprs (vec (map parse-long (str/split rest #"\s+")))]
                         [target exprs])))]
 
-  [(time (parallel-solve data [\+ \*]))
-   (time (parallel-solve data [\+ \* \|]))])
+  [(time (solve! data #{\+ \*}))
+   (time (solve! data #{\+ \* \|}))])
