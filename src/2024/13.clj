27cab5cd54c69202c5e7fccc3239cecdda4b7125
moni <lythe1107@gmail.com>
2024-12-13 15:12:36 +0800

    remove binding

--- src/2024/13.clj
+++ src/2024/13.clj
@@ -10,16 +10,15 @@
                  (when-let [[_ x y] (re-matches #"Prize: X=(\d+), Y=(\d+)" line)]
                    [(+ (parse-double x) offset) (+ (parse-double y) offset)]))))
        (partition 3)
-       (pmap (fn [[v1 v2 p]] {:v1 v1 :v2 v2 :p p}))))
+       (pmap (fn [[a b p]] {:a a :b b :p p}))))
 
-(defn find-solution [{[x1 y1] :v1 [x2 y2] :v2 [px py] :p}]
+(defn solve [{[x1 y1] :a [x2 y2] :b [px py] :p}]
   (let [t1 (/ (- (* px y2) (* x2 py)) (- (* x1 y2) (* x2 y1)))
         t2 (/ (- (* x1 py) (* px y1)) (- (* x1 y2) (* x2 y1)))]
     (when (every? #(= % (math/floor %)) [t1 t2])
       (+ (* t1 3.0) t2))))
 
-(let [data (slurp "inputs/2024/13.txt")]
-  (->> [0 10000000000000]
-       (pmap #(parse-input data %))
-       (pmap #(->> % (keep find-solution) (reduce +)))
-       (mapv long)))
+(->> [0 10000000000000]
+     (pmap #(parse-input (slurp "inputs/2024/13.txt") %))
+     (pmap #(->> % (keep solve) (reduce +)))
+     (mapv long))
