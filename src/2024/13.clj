786ec3da727fff262449802552e537eed71775a5
moni <lythe1107@gmail.com>
2024-12-22 18:43:54 +0800

    d13 make smaller

--- src/2024/13.clj
+++ src/2024/13.clj
@@ -3,17 +3,13 @@
 (defn parse [input offset]
   (->> (remove str/blank? (str/split-lines input))
        (pmap #(let [[x y] (pmap parse-double (re-seq #"\d+" %))]
-                (if (str/starts-with? % "Prize")
-                  [(+ x offset) (+ y offset)]
-                  [x y])))
+                (if (str/starts-with? % "Prize") [(+ x offset) (+ y offset)] [x y])))
        (partition 3)
        (pmap #(zipmap [:a :b :p] %1))))
 
 (defn solve [{[ax ay] :a [bx by] :b [px py] :p}]
   (let [x (/ (- (* px by) (* bx py)) (- (* ax by) (* bx ay)))
         y (/ (- (* ax py) (* px ay)) (- (* ax by) (* bx ay)))]
-    (when (every? #(zero? (mod % 1)) [x y])
-      (long (+ (* x 3) y)))))
+    (when (every? #(zero? (mod % 1)) [x y]) (long (+ (* x 3) y)))))
 
-(->> [0 10000000000000]
-     (pmap #(->> % (parse (slurp "inputs/2024/13.txt")) (keep solve) (reduce +))))
+(->> [0 10000000000000] (pmap #(->> % (parse (slurp "inputs/2024/13.txt")) (keep solve) (reduce +))))
