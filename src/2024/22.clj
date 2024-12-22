93972bfa7693835ce800c3ef96c43e2cc3bd6a11
moni <lythe1107@gmail.com>
2024-12-22 15:59:58 +0800

    d22 brevity

--- src/2024/22.clj
+++ src/2024/22.clj
@@ -6,15 +6,12 @@
       (#(bit-and (bit-xor % (bit-shift-right % 5)) 16777215))
       (#(bit-and (bit-xor % (bit-shift-left % 11)) 16777215))))
 
-(defn keep-first [m [p price]]
-  (if (contains? m p) m (assoc m p price)))
-
 (defn bananas [secret]
-  (->> (iterate step secret)
-       (take 2001)
-       (map #(mod % 10))
-       (#(reduce keep-first {} (map vector (partition 4 1 (map - (rest %) %)) (drop 4 %))))))
+  (letfn [(keep-first [m [p price]] (if (contains? m p) m (assoc m p price)))]
+    (->> (iterate step secret) (take 2001) (map #(mod % 10))
+         (#(reduce keep-first {} (map vector (partition 4 1 (map - (rest %) %)) (drop 4 %)))))))
 
 (let [secrets (->> "inputs/2024/22.txt" slurp str/split-lines (mapv parse-long))]
   [(time (->> secrets (pmap #(nth (iterate step %) 2000)) (reduce +)))
-   (time (->> secrets (pmap bananas) (apply merge-with +) vals (apply max)))])
+   (time (->> secrets (partition-all (/ (count secrets) 10))
+              (pmap #(apply merge-with + (map bananas %))) (apply merge-with +) vals (apply max)))])
