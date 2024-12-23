388c56e40906752abbe9afd82a409cca2c25b536
moni <lythe1107@gmail.com>
2024-12-23 17:12:28 +0800

    d23 transients

--- src/2024/23.clj
+++ src/2024/23.clj
@@ -2,8 +2,8 @@
          '[clojure.set :as set])
 
 (defn parse-graph [input]
-  (->> (map #(str/split % #"-") (str/split-lines input))
-       (reduce (fn [g [a b]] (merge-with into g {a #{b} b #{a}})) {})))
+  (transduce (comp (map #(str/split % #"-")) (map (fn [[a b]] {a #{b} b #{a}})))
+             (partial merge-with into) {} (str/split-lines input)))
 
 (defn triplets [g]
   (for [[a bs] g, b bs, c (g b) :when (and (pos? (compare b a)) (pos? (compare c b)) (contains? (g a) c))]
@@ -14,11 +14,14 @@
   ([g r p x]
    (if (and (empty? p) (empty? x))
      [r]
-     (let [v (set/difference p (g (first (set/union p x))))]
-       (->> v
-            (mapcat #(let [a (g %)] (bron-kerbosch g (conj r %) (set/intersection p a) (set/intersection x a))))
-            (#(if (seq v) % (bron-kerbosch g r (disj p (first v)) (conj x (first v))))))))))
+     (let [u (first (set/union p x)) v (set/difference p (g u))]
+       (if (seq v)
+         (persistent!
+          (reduce
+           #(reduce conj! %1 (bron-kerbosch g (conj r %2) (set/intersection p (g %2)) (set/intersection x (g %2))))
+           (transient []) v))
+         (bron-kerbosch g r (disj p u) (conj x u)))))))
 
 (let [graph (-> "inputs/2024/23.txt" slurp parse-graph)]
-  [(-> graph triplets (->> (filter #(some (fn [s] (str/starts-with? s "t")) %)) count))
+  [(time (-> graph triplets (->> (filter #(some (fn [s] (str/starts-with? s "t")) %)) count)))
    (time (-> graph bron-kerbosch (->> (apply max-key count) sort (str/join ","))))])
