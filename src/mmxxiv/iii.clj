cb4f5021bca805cfdd9142c9350a807c94d04a00
moni-dz <lythe1107@gmail.com>
2024-12-25 06:11:57 +0800

    d3 refactor

--- src/mmxxiv/iii.clj
+++ src/mmxxiv/iii.clj
@@ -1,28 +1,19 @@
 (ns mmxxiv.iii)
 
-(defn process-muls [input]
-  (->> input
-       (re-seq #"mul\(([-+]?\d+),([-+]?\d+)\)")
-       (map (fn [[_ x y]] (* (parse-long x) (parse-long y))))
-       (reduce +)))
-
 (defn process-instructions [input]
-  (let [tokens (re-seq #"do\(\)|don't\(\)|mul\([0-9]+,[0-9]+\)" input)]
-    (loop [tokens tokens
-           total 0
-           enabled? true]
-      (if-let [token (first tokens)]
-        (case token
-          "do()" (recur (rest tokens) total true)
-          "don't()" (recur (rest tokens) total false)
-          (recur (rest tokens)
-                 (if enabled?
-                   (+ total (reduce * (map parse-long (re-seq #"\d+" token))))
-                   total)
-                 enabled?))
-        total))))
+  (->> (re-seq #"do\(\)|don't\(\)|mul\([0-9]+,[0-9]+\)" input)
+       (reduce (fn [{:keys [total enabled?]} token]
+                 (case token
+                   "do()" {:total total :enabled? true}
+                   "don't()" {:total total :enabled? false}
+                   {:total (if enabled? (+ total (reduce * (map parse-long (re-seq #"\d+" token)))) total)
+                    :enabled? enabled?}))
+               {:total 0 :enabled? true})
+       :total))
 
-(let [data (slurp "inputs/2024/3.txt")]
-  [(process-muls data) (process-instructions data)])
+(let [input (slurp "inputs/2024/3.txt")]
+  [(transduce (comp (map #(* (parse-long (nth % 1)) (parse-long (nth % 2))))) + 0
+              (re-seq #"mul\(\s*(\d+)\s*,\s*(\d+)\s*\)" input))
+   (process-instructions input)])
 
 
