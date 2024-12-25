1ea41a5be9f4b35b5d56fa6e76f8fed0af8972ae
moni-dz <lythe1107@gmail.com>
2024-12-25 17:52:45 +0800

    d25 cleaner pipe

--- src/mmxxiv/xxv.clj
+++ src/mmxxiv/xxv.clj
@@ -1,11 +1,12 @@
 (ns mmxxiv.xxv
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]))
 
-(letfn [(parse [s] (->> s str/split-lines (map #(mapv {\# 1 \. 0} %)) (apply map +)))]
-  (->> "inputs/2024/25.txt" slurp str/trim (#(str/split % #"\n\n"))
-       ((juxt #(filter (comp #{\.} first) %) #(filter (comp #{\#} first) %)))
-       (map #(map parse %))
-       ((fn [[ks ls]]
-          (->> ks
-               (mapcat #(filter (fn [lock] (every? (fn [[x y]] (<= (+ x y) 7)) (map vector lock %))) ls))
-               count)))))
+(->> "inputs/2024/25.txt"
+     slurp
+     (#(str/split % #"\n\n"))
+     (map #(str/replace % "\n" ""))
+     ((juxt #(filter (comp #{\#} first) %) #(filter (comp #{\.} first) %)))
+     (apply combo/cartesian-product)
+     (filter (fn [[l k]] (not-any? #(= [\# \#] %) (map vector l k))))
+     count)
